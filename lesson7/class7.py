5b59a8ebb20d3d998097e9e17d73cd2afcb783f1
Rashid Bash <bae_info@mail.ru>
2020-05-08 20:05:01 -0500

    class 7 vstavkami

--- lesson7/class7.py
+++ lesson7/class7.py
@@ -4,13 +4,47 @@ size = 10
 array = [i for i in range(size)]
 random.shuffle(array)
 print(array)
+print('-'*50)
+#
+#Puzyrkom
+# n = 1
+# while n < len(array):
+#     for i in range(len(array) - n):
+#         if array[i] > array[i + 1]:
+#             array[i], array[i + 1] = array[i + 1], array[i]
+#     n += 1
+#     print(array)
+#
+# print(array)
 
-n = 1
-while n < len(array):
-    for i in range(len(array) - n):
-        if array[i] > array[i + 1]:
-            array[i], array[i + 1] = array[i + 1], array[i]
-    n += 1
-    print(array)
+# #Vyborom
+# def selection_sort(array):
+#
+#     for i in range(len(array)):
+#         idx_min = i
+#
+#         for j in range(i + 1, len(array)):
+#             if array[j] < array[idx_min]:
+#                 idx_min = j
+#
+#         array[idx_min], array[i] = array[i], array[idx_min]
+#         print(array)
+#
+# selection_sort(array)
+# print(array)
 
+#Vstavkami
+def insertion_sort(array):
+
+    for i in range(len(array)):
+        spam = array[i]
+        j = i
+
+        while array[j - 1] > spam and j > 0:
+            array[j] = array[j - 1]
+            j -=1
+
+        array[j] = spam
+
+insertion_sort(array)
 print(array)
