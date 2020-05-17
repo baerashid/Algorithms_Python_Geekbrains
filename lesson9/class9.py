9663a8b1fabecb80aacd25cc7fe1ee0a1074bf9a
Rashid Bash <bae_info@mail.ru>
2020-05-16 22:43:17 -0500

    part2

--- lesson9/class9.py
+++ lesson9/class9.py
@@ -1,26 +1,47 @@
-from binarytree import tree, bst, Node, build
+# from binarytree import tree, bst, Node, build
+#
+# class MyNode:
+#
+#     def __init__(self, data, left=None, right=None):
+#         self.data = data
+#         self.left = left
+#         self.right = right
+#
+# a = tree(height=4, is_perfect=False)
+# print(a)
+#
+# b = bst(height=3, is_perfect=True)
+# print(b)
+#
+# c = Node(7)
+# c.left = Node(3)
+# c.right = Node(11)
+# c.left.left = Node(1)
+# c.left.right = Node(5)
+# c.right.left = Node(9)
+# c.right.right = Node(13)
+# print(c)
+#
+# d = build([7, 3, 11, 1, 5, 9, 3, None, 2, None, 6])
+# print(d)
 
-class MyNode:
+from binarytree import bst
 
-    def __init__(self, data, left=None, right=None):
-        self.data = data
-        self.left = left
-        self.right = right
+def search(bin_search_tree, number, path=''):
+    if bin_search_tree.value == number:
+        return f'Число {number} обнаружено по следующему пути:\nКорень{path}'
 
-a = tree(height=4, is_perfect=False)
-print(a)
+    if number < bin_search_tree.value and bin_search_tree.left != None:
+        return search(bin_search_tree.left, number, path=f'{path}\nШаг влево <-')
 
-b = bst(height=3, is_perfect=True)
-print(b)
+    if number > bin_search_tree.value and bin_search_tree.right != None:
+        return search(bin_search_tree.right, number, path=f'{path}\nШаг вправо ->')
 
-c = Node(7)
-c.left = Node(3)
-c.right = Node(11)
-c.left.left = Node(1)
-c.left.right = Node(5)
-c.right.left = Node(9)
-c.right.right = Node(13)
-print(c)
+    return f'Number {number} is not found in the tree'
 
-d = build([7, 3, 11, 1, 5, 9, 3, None, 2, None, 6])
-print(d)
+
+
+bt = bst(height=5, is_perfect=False)
+print(bt)
+num = int(input('Введите число для поиска:  '))
+print(search(bt, num))
