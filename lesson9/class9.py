182c33f4a7472a30b84de4f28453d62a2c1efe61
Rashid Bash <bae_info@mail.ru>
2020-05-16 23:03:56 -0500

    part3

--- lesson9/class9.py
+++ lesson9/class9.py
@@ -24,24 +24,64 @@
 #
 # d = build([7, 3, 11, 1, 5, 9, 3, None, 2, None, 6])
 # print(d)
+#
+# from binarytree import bst
+#
+# def search(bin_search_tree, number, path=''):
+#     if bin_search_tree.value == number:
+#         return f'Число {number} обнаружено по следующему пути:\nКорень{path}'
+#
+#     if number < bin_search_tree.value and bin_search_tree.left != None:
+#         return search(bin_search_tree.left, number, path=f'{path}\nШаг влево <-')
+#
+#     if number > bin_search_tree.value and bin_search_tree.right != None:
+#         return search(bin_search_tree.right, number, path=f'{path}\nШаг вправо ->')
+#
+#     return f'Number {number} is not found in the tree'
+#
+#
+#
+# bt = bst(height=5, is_perfect=False)
+# print(bt)
+# num = int(input('Введите число для поиска:  '))
+# print(search(bt, num))
 
-from binarytree import bst
+# h_list = [None]* 26
+#
+# def my_append(value):
+#     index = ord(value[0]) - ord('a')
+#     h_list[index] = value
+#     print(h_list)
+#
+#
+# a = 'apricot'
+# my_append(a)
+#
+# b = 'banana'
+# my_append(b)
+#
+# c = 'apple'
+# my_append(c)
+#
+# def my_index(value):
+#
+#     letter = 26
+#     index = 0
+#     size = 10000
+#
+#     for i, char in enumerate(value):
+#         index += (ord(char) - ord('a') + 1) * letter ** i
+#
+#     return index % size
+#
+# print(my_index(a))
+# print(my_index(b))
+# print(my_index(c))
 
-def search(bin_search_tree, number, path=''):
-    if bin_search_tree.value == number:
-        return f'Число {number} обнаружено по следующему пути:\nКорень{path}'
-
-    if number < bin_search_tree.value and bin_search_tree.left != None:
-        return search(bin_search_tree.left, number, path=f'{path}\nШаг влево <-')
-
-    if number > bin_search_tree.value and bin_search_tree.right != None:
-        return search(bin_search_tree.right, number, path=f'{path}\nШаг вправо ->')
-
-    return f'Number {number} is not found in the tree'
+import hashlib
 
 
+print(hashlib.sha1(b'Hello World!').hexdigest())
+print(hashlib.sha1(b'Hello World.').hexdigest())
 
-bt = bst(height=5, is_perfect=False)
-print(bt)
-num = int(input('Введите число для поиска:  '))
-print(search(bt, num))
+print(hashlib.sha1(b'codeword' + b'Hello World.').hexdigest())
